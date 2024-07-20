bd0f4d133b23ed035c2dd7410b65d5a73ed7b56e
rahul2007prasad <107prasad107@gmail.com>
2024-07-20 11:07:38 +0530

    AWS S3 storage implementation

---
+++ src/main/java/com/rahul/aws/s3/AwsS3SbApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.aws.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3SbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3SbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/aws/s3/AwsS3SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.aws.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
